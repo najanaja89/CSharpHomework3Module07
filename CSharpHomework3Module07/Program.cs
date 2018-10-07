5239c6f12f67eea4839569bdae3947f3efaa8cd5
NajaNaja <NajaNaja89@gmail.com>
2018-10-07 16:56:42 +0600

    module 7 done

--- CSharpHomework3Module07/Program.cs
+++ CSharpHomework3Module07/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using MyClassLib.WordOfTanks;
 
 namespace CSharpHomework3Module07
@@ -11,18 +12,104 @@ namespace CSharpHomework3Module07
     {
         static void Main(string[] args)
         {
-            MyClassLib
+            Random rand = new Random();
+            int countPantera = 0;
+            int countT34 = 0;
+            int side = 1;
             string menu = "";
+            int size = 5;
             Console.WriteLine("Press 1 to start Tank Game");
             Console.WriteLine("Press any key to exit");
             menu = Console.ReadLine();
 
             if (menu == "1")
             {
-                
-                Tank t1 = new Tank;
-                Tank[] T-34 = new Tank[5];
+                Tank[] T34 = new Tank[size];
+                Tank[] Pantera = new Tank[size];
+
+                Console.WriteLine();
+                Console.WriteLine("Calculating Tanks Parameters. Please wait");
+                for (int i = 0; i < T34.Length; i++)
+                {
+                    T34[i] = new Tank("T34 " + (i + 1));
+                    Thread.Sleep(rand.Next(850, 950));
+                    Console.Write('*');
+                    Console.Write(' ');
+                }
+                for (int i = 0; i < Pantera.Length; i++)
+                {
+                    Pantera[i] = new Tank("Pantera " + (i + 1));
+                    Thread.Sleep(rand.Next(600, 800));
+                    Console.Write('*');
+                    Console.Write(' ');
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Choose your side: ");
+                Console.WriteLine("1 - T34 ");
+                Console.WriteLine("2 - Pantera ");
+                Console.WriteLine("Any key for random ");
+                string menuSide = Console.ReadLine();
+                if (menuSide == "1")
+                {
+                    side = 1;
+                }
+                else if (menuSide == "2")
+                {
+                    side = 2;
+                }
+                else
+                {
+                    side = rand.Next(1, 3);
+                    Console.WriteLine("Your side is " + side);
+                }
+                Console.WriteLine("Loading");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write('*');
+                    Console.Write(' ');
+                    Thread.Sleep(350);
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+
+                for (int i = 0; i < size; i++)
+                {
+                    string result = T34[i] * Pantera[i];
+                    Console.WriteLine(result);
+                    Console.WriteLine();
+                    if (result.Contains("T34"))
+                    {
+                        countT34++;
+                    }
+                    else if (result.Contains("Pantera"))
+                    {
+                        countPantera++;
+                    }
+                }
+                Console.WriteLine();
+
+                if (side == 1 && countT34 > countPantera)
+                {
+                    Console.WriteLine("You Win!");
+                }
+                else if (side == 2 && countT34 < countPantera)
+                {
+                    Console.WriteLine("You Win!");
+                }
+                else if ((side == 1 || side == 2) && countT34 == countPantera)
+                {
+                    Console.WriteLine("Draw!");
+
+                }
+                else Console.WriteLine("You Lose!");
+
             }
+
+            else Environment.Exit(0);
+
+            Console.ReadLine();
 
         }
     }
--- MyClassLib/WordOfTanks/Tank.cs
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 
 namespace MyClassLib.WordOfTanks
 {
-    class Tank
+    public class Tank
     {
         private Random rand = new Random();
-        private string Name { set; get; }
+        public string Name { set; get; }
         private int ammo;
         private int armor;
         private int mobility;
 
-        string GetAmmo()
+        public string GetAmmo()
         {
             return ammo.ToString();
         }
-        string GetArmor()
+        public string GetArmor()
         {
             return armor.ToString();
         }
-        string GetMobility()
+        public string GetMobility()
         {
             return mobility.ToString();
         }
@@ -32,19 +32,19 @@ namespace MyClassLib.WordOfTanks
         public Tank(string name)
         {
             Name = name;
-            ammo = rand.Next(0, 100);
-            armor = rand.Next(0, 100);
-            mobility = rand.Next(0, 100);
+            ammo = rand.Next(0, 101);
+            armor = rand.Next(0, 101);
+            mobility = rand.Next(0, 101);
         }
 
         public static string operator *(Tank t1, Tank t2)
         {
-            Tank t3 = new Tank("Winner");
 
             if (t1.mobility > t2.mobility && t1.ammo > t2.ammo)
             {
                 return t1.Name;
             }
+
             else if (t1.mobility > t2.mobility && t1.armor > t2.armor)
             {
                 return t1.Name;
@@ -61,11 +61,12 @@ namespace MyClassLib.WordOfTanks
                 return result;
             }
 
-            else if (t1.mobility == t2.mobility && t1.armor == t2.armor)
+            else if (t1.ammo == t2.ammo && t1.mobility == t2.mobility)
             {
                 string result = "draw!";
                 return result;
             }
+
 
             else return t2.Name;
 
